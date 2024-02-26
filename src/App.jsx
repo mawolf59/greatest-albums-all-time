2f94761fbc71113488a8e7be00b0a650cce78e98
Matt Wolf <matthew.wolf1121@gmail.com>
2024-02-26 18:56:40 -0500

    fixing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import albumsArray from './albums.json'
+import albumArray from './albums.json'
 import Album from './components/Album'
 
 const App = () => {
@@ -7,18 +7,7 @@ const App = () => {
     <div className="App">
       <h1>Rolling Stone: Greatest Albums of All Time</h1>
       <main>
-        {albumsArray.map((album) => (
-          <Album
-            key={album.id}
-            title={album.title}
-            artist={album.artist}
-            art={album.album - art}
-            year={album.year}
-            length={album.length}
-            genres={album.genres}
-            label={album.label}
-          />
-        ))}
+        <Album />
       </main>
     </div>
   )
--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,17 +1,9 @@
 const Album = (props) => {
   return (
-    <div>
-      <img src={props.art} alt={props.title} />
-      <h1>{props.title}</h1>
-      <h3>{props.artist}</h3>
-      <h5>{props.year}</h5>
-      <h5>{props.length}</h5>
-      <h5>{props.label}</h5>
-      {props.genres.map((genre) => (
-        <Genre key={genre.genreArray} />
-      ))}
+    <div className="album_info">
+      <h1>hello</h1>
     </div>
   )
 }
 
-export default Movie
+export default Album
