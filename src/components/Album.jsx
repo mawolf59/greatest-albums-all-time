9b2ada41d3e70c358f900dc5c70e783a828113c4
Matt Wolf <matthew.wolf1121@gmail.com>
2024-02-28 17:05:55 -0500

    artwork added and reformat details

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -4,13 +4,17 @@ const Album = (props) => {
       <h1 className="title">
         {props.id}. "{props.title}"
       </h1>
-      <h2>{props.artist}</h2>
-      <h3>
-        {props.year}, {props.label}, {props.genre}
-      </h3>
-      <h5>
-        Album Length: {props.minutes}: {props.seconds}
-      </h5>
+      <h2 className="artist">{props.artist}</h2>
+      <img className="album_art" src={props.art} alt={props.title} />
+      <div className="album_details">
+        <h4>
+          Label: {props.label}; {props.year}
+        </h4>
+        <h4>{props.genre}</h4>
+        <h5>
+          Album Length: {props.minutes}: {props.seconds}
+        </h5>
+      </div>
     </div>
   )
 }
